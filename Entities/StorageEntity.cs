ef98de4df1af11e4a06b992ca0a009c68c0ea49d
Enrique Hernandez <enrique_inf@hotmail.com>
2021-04-05 22:48:47 -0500

    first commit

---
+++ Entities/StorageEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities
+{
+    class StorageEntity
+    {
+        [Key]
+        [StringLength(50)]
+        public string StorageId { get; set; }
+
+        [Required]
+        public DateTime LastUpdate { get; set; }
+
+        public int PartialQuantity { get; set; }
+    }
+}
---
+++ Entities/WherehouseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities
+{
+    class WherehouseEntity
+    {
+        [Key]
+        [StringLength(50)]
+        public string WherehouseId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string WharehouseName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string WherehouseAddress { get; set; }
+    }
+}

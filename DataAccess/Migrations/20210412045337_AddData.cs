408a0cc7de623e7f66c899b039cc7e715ff0586f
Enrique Hernandez <enrique_inf@hotmail.com>
2021-04-11 23:56:51 -0500

    DB migration

---
+++ DataAccess/Migrations/20210412045337_AddData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class AddData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { "001", "Limpieza para el hogar" },
+                    { "002", "Salud" },
+                    { "003", "Video juegos" },
+                    { "004", "Cuidado personal" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Warehouses",
+                columns: new[] { "WarehouseId", "WarehouseAddress", "WarehouseName" },
+                values: new object[,]
+                {
+                    { "be9cc259-2326-40b2-94f9-fb6701ce90db", "Calle 8 con 23", "Bodega Central" },
+                    { "b14a705f-c14b-4bf4-8e9e-b1239d956dee", "San Martin Obispo", "Bodega Norte" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: "001");
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: "002");
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: "003");
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: "004");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "b14a705f-c14b-4bf4-8e9e-b1239d956dee");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "be9cc259-2326-40b2-94f9-fb6701ce90db");
+        }
+    }
+}

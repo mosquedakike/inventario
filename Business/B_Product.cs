51d255a208424975ae152ebc9bcd08e9841f2179
Luis Enrique Hernandez Mosqueda <enrique_inf@hotmail.com>
2021-04-13 15:38:47 -0500

    ending CRUD

--- Business/B_Product.cs
+++ Business/B_Product.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Entities;
+using DataAccess;
+using System.Linq;
 
 namespace Business
 {
     public class B_Product
     {
+        public List<ProductEntity> ProductyList()
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Products.ToList();
+            }
+        }
 
+        public void CreateProduct(ProductEntity oProduct)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Products.Add(oProduct);
+                db.SaveChanges();
+            } 
+        }
+
+        public void UpdateProduct(ProductEntity oProduct)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Products.Update(oProduct);
+                db.SaveChanges();
+            }
+        }
     }
 }
--- Business/B_Warehouse.cs
+++ Business/B_Warehouse.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Entities;
+using DataAccess;
+using System.Linq;
 
 namespace Business
 {
     public class B_Warehouse
     {
+        public List<WarehouseEntity> WarehoseList()
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Warehouses.ToList();
+            }
+        }
 
+        public void CreateWarehouse(WarehouseEntity oWarehouse)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Warehouses.Add(oWarehouse);
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateWarehouse(WarehouseEntity oWarehouse)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Warehouses.Update(oWarehouse);
+                db.SaveChanges();
+            }
+        }
     }
 }

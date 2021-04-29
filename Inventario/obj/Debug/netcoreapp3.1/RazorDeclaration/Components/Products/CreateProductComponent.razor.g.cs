3c70e010e050a5119940d9ca1eb3f112a5d4f7ff
Enrique Hernandez <enrique_inf@hotmail.com>
2021-04-28 22:34:53 -0500

    adding disabledInput

--- Inventario/obj/Debug/netcoreapp3.1/RazorDeclaration/Components/Products/CreateProductComponent.razor.g.cs
+++ Inventario/obj/Debug/netcoreapp3.1/RazorDeclaration/Components/Products/CreateProductComponent.razor.g.cs
@@ -13,112 +13,112 @@ namespace Inventario.Components.Products
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 1 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 2 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 3 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 4 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 5 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 6 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 7 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 8 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Inventario;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 9 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Inventario.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 10 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Inventario.Components.Ejercicios.BlazorPages;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 11 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Inventario.Components.Ejercicios.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 12 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Inventario.Components.Products;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 13 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Inventario.Components.Categories;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "C:\Users\enriq\source\repos\inventario\Inventario\_Imports.razor"
+#line 14 "C:\Users\enriq\source\repos\Inventario\Inventario\_Imports.razor"
 using Inventario.Components.Warehouses;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 1 "C:\Users\enriq\source\repos\inventario\Inventario\Components\Products\CreateProductComponent.razor"
+#line 1 "C:\Users\enriq\source\repos\Inventario\Inventario\Components\Products\CreateProductComponent.razor"
 using Entities;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\enriq\source\repos\inventario\Inventario\Components\Products\CreateProductComponent.razor"
+#line 2 "C:\Users\enriq\source\repos\Inventario\Inventario\Components\Products\CreateProductComponent.razor"
 using Business;
 
 #line default
@@ -132,7 +132,7 @@ using Business;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 34 "C:\Users\enriq\source\repos\inventario\Inventario\Components\Products\CreateProductComponent.razor"
+#line 35 "C:\Users\enriq\source\repos\Inventario\Inventario\Components\Products\CreateProductComponent.razor"
        
     //Objeto de tipo ProductEntity
     ProductEntity oProduct = new ProductEntity();

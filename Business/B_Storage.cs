3a8e8a8522b1470871011c4b2d5d12a78c91d766
Enrique Hernandez <enrique_inf@hotmail.com>
2021-05-03 23:15:23 -0500

    adding InOut components

--- Business/B_Storage.cs
+++ Business/B_Storage.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Entities;
 using DataAccess;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Business
 {
@@ -26,14 +27,26 @@ namespace Business
             }
         }
 
-        public static bool IsProductInWarehouse(string idProduct)
+        public static bool IsProductInWarehouse(string idStorage)
         {
             using (var db = new InventaryContext())
             {
                 var product = db.Storages.ToList()
-                    .Where(s => s.ProductId == idProduct);
+                    .Where(s => s.StorageId == idStorage);
 
                 return product.Any();
+            }
+        }
+
+        public static List<StorageEntity> StorageProductsByWarehouse(string idWarehouse)
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Storages
+                    .Include(s => s.Product)
+                    .Include(s => s.Warehouse)
+                    .Where(s => s.WarehouseId == idWarehouse)
+                    .ToList();
             }
         }
 
